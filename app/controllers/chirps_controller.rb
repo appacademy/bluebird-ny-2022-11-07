fdeaca3e7b895f48a33d2ac72a9cdec8f7b39834
aycelacap <[raycel5lacap@gmail.com]>
2022-12-14 14:13:56 -0500

    Add routes and controllers

---
+++ app/controllers/chirps_controller.rb
@@ -0,0 +1,61 @@
+class ChirpsController < ApplicationController
+
+  def index
+    @chirps = Chirp.all 
+    # debugger
+    render json: @chirps
+  end
+
+  def show
+    @chirp = Chirp.find(params[:id])
+    # debugger
+    render json: @chirp
+  end
+
+  def create 
+    @chirp = Chirp.new(chirp_params)
+
+    # chirp[body] = ""
+    # chirp[title] = ""
+
+    @chirp.author = User.first 
+
+    # debugger
+    if @chirp.save
+      redirect_to chirp_url(@chirp)
+    else
+      render json: @chirp.errors.full_messages, status: 422
+    end
+  end
+
+  def update 
+    @chirp = Chirp.find(params[:id])
+
+    if @chirp.update(chirp_params)
+      redirect_to chirp_url(@chirp)
+    else 
+      render json: @chirp.errors.full_messages, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy 
+    @chirp = Chirp.find(params[:id])
+    # @chirp = Chirp.find_by(id: 5)
+    @chirp.destroy 
+    redirect_to chirps_url
+  end
+
+
+  private 
+  def chirp_params 
+    params.require(:chirp).permit(:body)
+  end
+
+  
+#   chirps GET    /chirps(.:format)     chirps#index
+#   chirps     POST   /chirps(.:format)     chirps#create
+#  chirp GET    /chirps/:id(.:format) chirps#show
+#   chirp     PATCH  /chirps/:id(.:format) chirps#update
+#    chirp    PUT    /chirps/:id(.:format) chirps#update
+#    chirp    DELETE /chirps/:id(.:format) chirps#destroy
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get '/chirps', to: "chirps#index"
+  # get '/chirps/:id', to: "chirps#show"
+  # post '/chirps', to: "chirps#create"
+  # patch '/chirps/:id', to: "chirps#update"
+  # put '/chirps/:id', to: "chirps#update"
+  # delete '/chirp/:id', to: "chirps#destroy"
+
+  # resources <controller>, <action_options>
+
+  resources :chirps, only: [:index, :show, :create, :update, :destroy]
+
+  # resources :chirps, except: [:new, :edit]
+
+
+
 end
